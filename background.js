17022b00871533e91c630e0358f485f9cf96b3e9
Mio (Diyar) <37794647+MioLoveyah@users.noreply.github.com>
2023-03-12 17:58:54 +0000

    Preset beta finished - Auto bump progress

---
+++ background.js
@@ -0,0 +1,22 @@
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Extension installed!');
+  });
+  
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete') {
+      setInterval(() => {
+        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                    func: () => {
+                    console.log(`Attemping bump.`);
+                    const buttons = document.querySelectorAll('.rlg-trade__action.rlg-trade__bump.--bump ');
+                    buttons.forEach((button) => {
+                    button.click();
+                    });
+                }
+          });
+        });
+      }, 10000);
+    }
+});
--- popup.js
+++ popup.js
@@ -1,79 +1,94 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('preset1').addEventListener('click', async () => {
-        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-        await chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            func: () => {
-                const hasElements = document.querySelector('.rlg-trade__itemshas ');
-                const wantElements = document.querySelector('.rlg-trade__itemswants ');
-                var hasItems = [];
-                var wantItems = [];
-
-                // Add each has item
-                while (hasElements.hasChildNodes()) {
-                    hasItems.push(hasElements.firstChild.outerHTML);
-                    hasElements.removeChild(hasElements.firstChild);
-                }
-
-                // Add each want item
-                while (wantElements.hasChildNodes()) {
-                    wantItems.push(wantElements.firstChild.outerHTML);
-                    wantElements.removeChild(wantElements.firstChild);
-                }
-                
-                // Validate items
-                hasItems = hasItems.filter( Boolean );
-                wantItems = wantItems.filter( Boolean );
-
-                // Store items
-                chrome.storage.local.set({
-                    'preset1': {
-                        hasItems: hasItems,
-                        wantItems: wantItems
+    for (var i = 1; i < 6; i ++) {
+        console.log(`Preset ${i} being setup.`)
+        document.getElementById(`preset${i}`).addEventListener('click', (function(i) {
+            return async () => {
+                let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+                await chrome.scripting.executeScript({
+                    target: { tabId: tab.id },
+                    args: [i],
+                    func: (i) => {
+                        const hasElements = document.querySelector('.rlg-trade__itemshas ');
+                        const wantElements = document.querySelector('.rlg-trade__itemswants ');
+                        var hasItems = [];
+                        var wantItems = [];
+ 
+                        // Add each has item
+                        while (hasElements.hasChildNodes()) {
+                            hasItems.push(hasElements.firstChild.outerHTML);
+                            hasElements.removeChild(hasElements.firstChild);
+                        }
+ 
+                        // Add each want item
+                        while (wantElements.hasChildNodes()) {
+                            wantItems.push(wantElements.firstChild.outerHTML);
+                            wantElements.removeChild(wantElements.firstChild);
+                        }
+ 
+                        // Validate items
+                        hasItems = hasItems.filter( Boolean );
+                        wantItems = wantItems.filter( Boolean );
+ 
+                        // Store items
+                        chrome.storage.local.set({
+                            [`preset${i}`]: {
+                                hasItems: hasItems,
+                                wantItems: wantItems
+                            }
+                        });
+ 
+                        // Re-add the items
+                        chrome.storage.local.get([`preset${i}`], (data) => {
+                            const parser = new DOMParser();
+                            data[`preset${i}`].hasItems.forEach(i => {
+                                hasElements.appendChild(parser.parseFromString(i, 'text/html').body.firstChild);
+                            });
+                            data[`preset${i}`].wantItems.forEach(i => {
+                                wantElements.appendChild(parser.parseFromString(i, 'text/html').body.firstChild);
+                            });
+                        });
                     }
-                }, function() {
-                    console.log('Settings saved');
-                });
-
-                // Re-add the items
-                chrome.storage.local.get('preset1', (data) => {
-                    const parser = new DOMParser();
-                    data['preset1'].hasItems.forEach(i => {
-                        hasElements.appendChild(parser.parseFromString(i, 'text/html').body.firstChild);
-                    });
-                    data['preset1'].wantItems.forEach(i => {
-                        wantElements.appendChild(parser.parseFromString(i, 'text/html').body.firstChild);
-                    });
                 });
             }
-        });
-    });
+        })(i));
+    }
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('preset2').addEventListener('click', async () => {
-        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-        await chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            func: () => {
-                const hasElements = document.querySelector('.rlg-trade__itemshas ');
-                const wantElements = document.querySelector('.rlg-trade__itemswants ');
-                while (hasElements.hasChildNodes()) {
-                    hasElements.removeChild(hasElements.firstChild);
-                }
-                while (wantElements.hasChildNodes()) {
-                    wantElements.removeChild(wantElements.firstChild);
-                }
-                chrome.storage.local.get('preset1', (data) => {
-                    const parser = new DOMParser();
-                    data['preset1'].hasItems.forEach(i => {
-                        hasElements.appendChild(parser.parseFromString(i, 'text/html').body.firstChild);
+    for (let i = 1; i < 6; i ++) {
+        document.getElementById(`restore${i}`).addEventListener('click', async () => {
+            let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+            await chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                args: [i],
+                func: (i) => {
+                    console.log(`Attempting fetch`);
+                    chrome.storage.local.get(`preset${i}`, (data) => {
+                        if (Object.keys(data).length === 0) {
+                            console.log(`Preset data does not exist.`);
+                            alert(`Preset data does not exist.`);
+                            return;
+                        } else {
+                            const hasElements = document.querySelector('.rlg-trade__itemshas ');
+                            const wantElements = document.querySelector('.rlg-trade__itemswants ');
+                            while (hasElements.hasChildNodes()) {
+                                hasElements.removeChild(hasElements.firstChild);
+                            }
+                            while (wantElements.hasChildNodes()) {
+                                wantElements.removeChild(wantElements.firstChild);
+                            }
+                            const parser = new DOMParser();
+                            data[`preset${i}`].hasItems.forEach(i => {
+                                hasElements.appendChild(parser.parseFromString(i, 'text/html').body.firstChild);
+                            });
+                            data[`preset${i}`].wantItems.forEach(i => {
+                                wantElements.appendChild(parser.parseFromString(i, 'text/html').body.firstChild);
+                            });
+                            console.log(`Success.`);
+                        }
                     });
-                    data['preset1'].wantItems.forEach(i => {
-                        wantElements.appendChild(parser.parseFromString(i, 'text/html').body.firstChild);
-                    });
-                });
-            }
+                }
+            });
         });
-    });
+    }
 });
